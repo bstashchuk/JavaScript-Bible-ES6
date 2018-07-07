e30949851db355df685176ce97f1923ca50dabfb
Bogdan <bstashchuk@gmail.com>
2018-07-07 22:02:26 +0300

    Finish

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 // // Scope
-// var a = 10;
+// let a = 10;
 // console.log(a);
 
 // function fn() {
-//   var b = true;
+//   let b = true;
 //   console.log(b);
 // }
 
 // fn();
 
-// console.log(b);
+// // console.log(b);
+// {
+//   let b1 = 1;
+// }
+
+// for (let j = 0; j < 5; j++) {
+//   console.log(j);
+// }
 
 // // Reassignment
-// var c = 1;
+// let c = 1;
 // c = 2;
 
 // function fn2() {
@@ -22,15 +29,32 @@
 // fn2();
 
 // // Redeclaration
-// var d = false;
-// var d = true;
+// let d = false;
+// // let d = true;
+// console.log(d);
 
 // function fn3() {
-//   var d = 5;
+//   let d = 5;
+//   console.log(d);
 // }
 
 // fn3();
 
+// {
+//   let d = "abc";
+//   console.log(d);
+// }
+
+// console.log(d);
+
 // // Hoisting
+// let e;
 // e = 5;
-// var e;
+
+// function fn4() {
+//   let f;
+//   f = 10;
+//   console.log(f);
+// }
+
+// fn4();
