806c49a2d72a5b5e08ff441e5b5a7b1585ff2db8
Bogdan <bstashchuk@gmail.com>
2018-07-07 10:54:52 +0300

    Final code

---
+++ index.js
@@ -0,0 +1,36 @@
+// // Scope
+// var a = 10;
+// console.log(a);
+
+// function fn() {
+//   var b = true;
+//   console.log(b);
+// }
+
+// fn();
+
+// console.log(b);
+
+// // Reassignment
+// var c = 1;
+// c = 2;
+
+// function fn2() {
+//   c = 3;
+// }
+
+// fn2();
+
+// // Redeclaration
+// var d = false;
+// var d = true;
+
+// function fn3() {
+//   var d = 5;
+// }
+
+// fn3();
+
+// // Hoisting
+// e = 5;
+// var e;
