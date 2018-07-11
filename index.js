c4ce51f3d06f51a2e21f1e7e0d8c565eaad09df9
Bogdan <bstashchuk@gmail.com>
2018-07-11 22:06:01 +0300

    Start

---
+++ index.js
@@ -0,0 +1,7 @@
+// 1. Make code readable for other developers
+
+// 2. Always declare variables before first usage
+
+// 3. Always add "use strict" at the beginning of each JS file
+
+// 4. Don't expose to the outer scopes locally used variables
