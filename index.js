ab7372c1831075639ad8ee60e6b1179f652eb021
Bogdan <bstashchuk@gmail.com>
2018-07-12 13:06:15 +0300

    Finish

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
-numbers1 = [23, 87, 110, 11, 20, 5, 34];
-numbers2 = [11, 21, 31];
+"use strict";
 
-var onlyOddNumbers = function(arr) {
-  oddNumbers = [];
-  EVEN_NUMBERS_QUANTITY = 0;
-  len = arr.length;
+const numbers1 = [23, 87, 110, 11, 20, 5, 34];
+const numbers2 = [11, 21, 31];
 
-  for (i = 0; i < len; i++) {
+const onlyOddNumbers = function(arr) {
+  var oddNumbers = [];
+  var evenNumbersQuantity = 0;
+  const LEN = arr.length;
+
+  for (let i = 0; i < LEN; i++) {
     arr[i] % 2
       ? oddNumbers.push(arr[i])
-      : EVEN_NUMBERS_QUANTITY++;
+      : evenNumbersQuantity++;
   }
 
-  var info;
-  if (EVEN_NUMBERS_QUANTITY === 0) {
-    info = "Array contains only odd numbers";
+  if (evenNumbersQuantity === 0) {
+    let info = "Array contains only odd numbers";
     console.log(info);
   } else {
-    info =
-      "There are " +
-      EVEN_NUMBERS_QUANTITY +
-      " even numbers";
+    let info =
+      "There are " + evenNumbersQuantity + " even numbers";
     console.log(info);
   }
 
   return {
     oddNumbers: oddNumbers,
-    EVEN_NUMBERS_QUANTITY: EVEN_NUMBERS_QUANTITY
+    evenNumbersQuantity: evenNumbersQuantity
   };
-
-  var oddNumbers;
 };
-
-var EVEN_NUMBERS_QUANTITY;
 
 console.log(onlyOddNumbers(numbers1));
 console.log(onlyOddNumbers(numbers2));
