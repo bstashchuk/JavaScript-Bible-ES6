050cc9e682012c38b1cec9693ba0ae441b7906e4
Bogdan <bstashchuk@gmail.com>
2018-07-12 12:27:33 +0300

    Edits

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 numbers1 = [23, 87, 110, 11, 20, 5, 34];
-numbers2 = [10, 20, 30];
+numbers2 = [11, 21, 31];
 
 var onlyOddNumbers = function(arr) {
   oddNumbers = [];
@@ -10,12 +10,25 @@ var onlyOddNumbers = function(arr) {
       ? oddNumbers.push(arr[i])
       : EVEN_NUMBERS_QUANTITY++;
   }
+  var info;
+  if (EVEN_NUMBERS_QUANTITY === 0) {
+    info = "Array contains only odd numbers";
+    console.log(info);
+  } else {
+    info =
+      "There are " +
+      EVEN_NUMBERS_QUANTITY +
+      " even numbers";
+    console.log(info);
+  }
   return {
     oddNumbers: oddNumbers,
     EVEN_NUMBERS_QUANTITY: EVEN_NUMBERS_QUANTITY
   };
-  var oddNumbers, EVEN_NUMBERS_QUANTITY;
+  var oddNumbers;
 };
+
+var EVEN_NUMBERS_QUANTITY;
 
 console.log(onlyOddNumbers(numbers1));
 console.log(onlyOddNumbers(numbers2));
