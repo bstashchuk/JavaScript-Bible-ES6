ace5fc117c373dff84b12b846801cf726069a441
Bogdan <bstashchuk@gmail.com>
2018-07-13 10:00:27 +0300

    Final

---
+++ index.js
@@ -0,0 +1,22 @@
+// // var without assignment
+// console.log(a); // undefined
+// var a;
+
+// // var with assignment
+// b = 20;
+// console.log(b); // 20
+// var b;
+
+// // var inside of the function
+// function fn() {
+//   console.log(c); // undefined
+//   var c;
+//   d = 10;
+//   console.log(d);
+//   var d;
+// }
+
+// // console.log(c); // c is not defined
+// // console.log(d); // d is not defined
+
+// fn();
