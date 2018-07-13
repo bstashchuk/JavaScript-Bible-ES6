df3346e82d4c07dc9e49a4ad8611205f49750367
Bogdan <bstashchuk@gmail.com>
2018-07-13 11:04:06 +0300

    Final

---
+++ index.js
@@ -0,0 +1,19 @@
+// global scope
+// console.log(a); // a is not defined
+let a; // a is initialized with "undefined"
+console.log(a); // undefined
+a = 10; // a is assigned and has value 10
+console.log(a); // 10
+
+// // function
+// function fn() {
+//   // console.log(a); // a is not defined
+//   let a; // a is initialized with "undefined"
+//   console.log(a); // undefined
+//   a = 10; // a is assigned and has value 10
+//   console.log(a); // 10
+// }
+
+// // console.log(a); // not defined
+
+// fn();
