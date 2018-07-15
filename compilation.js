2a3183550c82488dd7d6b2c115044dfc557d508c
Bogdan <bstashchuk@gmail.com>
2018-07-15 12:18:40 +0300

    Finish

---
+++ compilation.js
@@ -0,0 +1,21 @@
+/* COMPILATION PHASE
+Scopes:
+Global {
+  Line 16: 
+  var a; decleared and initialized with "undefined"
+
+}
+sum {
+
+}
+
+if {
+
+} else {
+
+}
+
+mult {
+
+}
+*/
---
+++ execution.js
@@ -0,0 +1,11 @@
+/* EXECUTION PHASE
+Line 1: 
+Global scope variable "a" becomes assigned.
+Value of the "a" is 2.
+
+Line 2:
+
+Line 4: 
+
+...
+*/
