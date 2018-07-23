e2e6feef72831f011ff674037241db5135abe5ff
Bogdan <bstashchuk@gmail.com>
2018-07-23 11:26:42 +0300

    Final

--- compilation.js
+++ compilation.js
@@ -1,21 +1,36 @@
 /* COMPILATION PHASE
 Scopes:
 Global {
+  Line 2:
+  const b; decleared, TDZ
+
+  Line4:
+  const sum; decleared, TDZ
+
   Line 16: 
   var a; decleared and initialized with "undefined"
 
+  Line 18:
+  function mult; decleared, initialized and assigned
 }
-sum {
 
+sum {
+ // nothing happens
 }
 
 if {
-
+  Line 9:
+  let multResult; decleared, TDZ
 } else {
-
+  Line 12:
+  let sumResult; decleared, TDZ
 }
 
 mult {
+  Line 18:
+  var a; decleared, initialized with "undefined"
 
+  Line 18:
+  var b; decleared, initialized with "undefined"
 }
 */
--- execution.js
+++ execution.js
@@ -4,8 +4,58 @@ Global scope variable "a" becomes assigned.
 Value of the "a" is 2.
 
 Line 2:
+Global scope const "b" becomes initialized and assigned.
+Value of the "b" is 5.
 
 Line 4: 
+Global scope const "sum" becomes initialized and assigned.
+Value of the "sum" is "function(a, b) {
+  return a + b;
+};".
+sum {
+  Line 4:
+  var a; decleared, initialized with "undefined"
+  var b; decleared, initialized with "undefined"
+}
+
+Line 8:
+"b" is already assigned and has value 5
+"a" is already assigned and has value 2
+(b > a) -> true
+{
+  Line 9:
+
+  Safe to call mult function (assigned in the global scope). "a" and "b" are assigned and have values 2 and 5.
+  mult(2, 5);
+
+  Line 18: (mult function execution)
+    Function scope:
+      a = 2; becomes assigned
+      b = 5; becomes assigned
+
+    Line 21:
+    var result; decleared and initialized with "undefined"
+
+    Line 19:
+    2 * 5 -> "result" becomes assigned and get value 10
+
+    Line 20:
+    10 is returned
+    *MULT FUNCTION CALL ENDS HERE*
+
+  Block scope let variable "multResult" becomes initialized and assigned.
+  "multResult" value is 10
+
+  Line 10:
+  Global Scope {
+    var finalResult = 10;  decleared, initialized and assigned.
+  }
+}
+
+Line 24:
+"finalResult" is found in the global scope and it's value is 10
+
+console.log(finalResult); -> 10 is printed
 
 ...
 */
