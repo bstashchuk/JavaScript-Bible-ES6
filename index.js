7a7c2c503c5ec492b61567b747ff8703765a45ec
Bogdan <bstashchuk@gmail.com>
2018-08-05 21:53:39 +0300

    Final

---
+++ index.js
@@ -0,0 +1,9 @@
+const postsJSON = [
+  '{"postId":1355,"commentsQuantity":5}',
+  '{"postId":5131,"commentsQuantity":13}',
+  '{"postId":6134,"commentsQuantity":2}'
+];
+
+/* From the "postsJSON" array create "posts" array that will consist of JavaScript objects */
+
+console.log(posts);
