4693fd2d004a2b80e2353915e897227c2ffbaae9
Bogdan <bstashchuk@gmail.com>
2018-08-05 18:23:42 +0300

    Final

---
+++ index.js
@@ -0,0 +1,11 @@
+const myCities = ["London", "New York", "Singapore"];
+
+/* Create a function called "arrayInfo" that will return strings like 
+"London is located at the index 0 in the myCities array" /*
+
+
+/* Iterate over the "myCities" array, call "arrayInfo" function for each of the elements and print result of the call to the console*/
+
+// Option 1: "for"
+
+// Option 2: "forEach"
