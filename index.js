a9a233706b04e19391cff0da370ddd7c47195546
Bogdan <bstashchuk@gmail.com>
2018-08-06 07:29:47 +0300

    Final

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ const postsJSON = [
 
 /* From the "postsJSON" array create "posts" array that will consist of JavaScript objects */
 
+// // for
+// var posts = [];
+// for (let i = 0; i < postsJSON.length; i++) {
+//   posts.push(JSON.parse(postsJSON[i]));
+// }
+
+// map - Option 1
+const posts = postsJSON.map(JSON.parse);
+
+// // map - Option 2
+// const posts = postsJSON.map(post => JSON.parse(post));
+
 console.log(posts);
+console.log(posts[0].postId); // 1355
