7f79cd7d80ead8b971f0df543bdaa9751f28d8aa
Bogdan <bstashchuk@gmail.com>
2018-08-06 08:24:46 +0300

    Final

---
+++ index.js
@@ -0,0 +1,13 @@
+const posts = [
+  { postId: 1355, commentsQuantity: 5 },
+  { postId: 5131, commentsQuantity: 13 },
+  { postId: 6134, commentsQuantity: 2 }
+];
+
+/* Create a function "findSinglePost" that will have two parameters - "postId" and "posts" and will return object with matched "postId".
+If post wasn't found - return "undefined"
+*/
+
+console.log(findSinglePost(6134, posts)); // { postId: 6134, commentsQuantity: 2 }
+
+console.log(findSinglePost(4511, posts)); // undefined
