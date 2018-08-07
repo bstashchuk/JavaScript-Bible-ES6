91e9b63f0f74f9a983566395bce4c9d42e8dab6a
Bogdan <bstashchuk@gmail.com>
2018-08-07 11:50:07 +0300

    Final

---
+++ index.js
@@ -0,0 +1,12 @@
+const transport = ["Bus", "Car", "Bicycle", "Airplane"];
+
+/* Create a function "elementFound" with two parameters "array" and "element".
+If "element" is found in the "array" - return "true".
+Otherwise return "false"
+
+Hint: Use "some" helper method to solve this
+*/
+
+console.log(elementFound(transport, "Bus")); // true
+console.log(elementFound(transport, "Phone")); // false
+console.log(elementFound(transport, "Airplane")); // true
