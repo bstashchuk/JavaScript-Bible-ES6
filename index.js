1d3c1163b4b6c366fd8c1c6425af28007e262082
Bogdan <bstashchuk@gmail.com>
2018-08-07 11:24:51 +0300

    Final

---
+++ index.js
@@ -0,0 +1,21 @@
+const a = [5, "abc", 10, 1];
+const b = [4, 10, 14, 25, 25, 50];
+const c = [150, 132, 80, 40];
+const d = [15, 26, 10, 23, 85];
+
+/*
+Create a function "arrayCheck" with one parameter - "array".
+
+If at least one element in the array is not a number - return "Some elements are not numbers".
+
+If numbers in the array are sorted in ascending order - return "Array is sorted is ascending order".
+
+If numbers in the array are sorted in descending order - return "Array is sorted is descending order".
+
+If array is not sorted - return "Array is not sorted"
+*/
+
+console.log(arrayCheck(a)); // Some elements are not numbers
+console.log(arrayCheck(b)); // Array is sorted is ascending order
+console.log(arrayCheck(c)); // Array is sorted is descending order
+console.log(arrayCheck(d)); // Array is not sorted
