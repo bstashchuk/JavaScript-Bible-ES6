20cdb1b9694d56ff1c0b81a4afc0da3b3f4e49ea
Bogdan <bstashchuk@gmail.com>
2018-08-08 12:36:15 +0300

    Final

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ Otherwise return "false"
 Hint: Use "some" helper method to solve this
 */
 
+const elementFound = (inputArray, searchElement) =>
+  inputArray.some(element => element === searchElement);
+
 console.log(elementFound(transport, "Bus")); // true
 console.log(elementFound(transport, "Phone")); // false
 console.log(elementFound(transport, "Airplane")); // true
