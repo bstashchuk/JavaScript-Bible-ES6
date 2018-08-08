360d439d48cd0e3d0021c36a1f379eb052e983da
Bogdan <bstashchuk@gmail.com>
2018-08-08 13:23:14 +0300

    Final

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const a = [1, 2, 3];
 const b = [1, 2, 3];
 
-console.log(a === b); // false
+const a1 = a;
+// a.push(4);
+
+console.log(a === b); // false. Different pointers
+console.log(a === a1); // true. Because pointers are equals
 
 /* Create a function "arraysAreEqual" with two parameters "arrayOne" and "arrayTwo". 
 
@@ -9,8 +13,16 @@ If two arrays are equal (have same quantity of the elements and all elements mat
 
 Otherwise return "false". */
 
+const arraysAreEqual = (arrayOne, arrayTwo) =>
+  arrayOne.length === arrayTwo.length &&
+  arrayOne.every(
+    (element, index) => element === arrayTwo[index]
+  );
+
 const c = [2, 1, 3];
 const d = [1, 2, 3, 4];
+
+console.log("_____");
 
 console.log(arraysAreEqual(a, b)); // true
 console.log(arraysAreEqual(a, c)); // false
