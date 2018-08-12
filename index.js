26be46999104798372e85e69ba50266f9deabd9a
Bogdan <bstashchuk@gmail.com>
2018-08-12 09:10:42 +0300

    Finish

---
+++ index.js
@@ -0,0 +1,26 @@
+const posts = [
+  {
+    title: "How to find JavaScript developer job?",
+    postId: 3421,
+    comments: 25
+  },
+  {
+    title: "Is it hard to learn ES6?",
+    postId: 5216,
+    comments: 3
+  },
+  {
+    title: "Should I learn React or Angular?",
+    postId: 8135,
+    comments: 12
+  }
+];
+
+const minimalComentsQty = 10;
+
+/* Create a function "popularPostsIds" with two parameters "posts" and "minimalComentsQty".
+
+This function "popularPostsIds" should return an array of postIds that have quantity of "comments" at least equal to "minimalComentsQty"
+*/
+
+console.log(popularPostsIds(posts, minimalComentsQty)); // [3421, 8135]
