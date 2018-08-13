e1384a67e0cc9e19b9c35bd3bae61de55a5b4bf2
Bogdan <bstashchuk@gmail.com>
2018-08-13 09:15:04 +0300

    Final

---
+++ index.js
@@ -0,0 +1,38 @@
+const products = [
+  {
+    title: "Phone case",
+    price: 23,
+    quantity: 2,
+    category: "Accessories"
+  },
+  {
+    title: "Android phone",
+    price: 150,
+    quantity: 1,
+    category: "Phones"
+  },
+  {
+    title: "Headphones",
+    price: 78,
+    quantity: 1,
+    category: "Accessories"
+  },
+  {
+    title: "Sport Watch",
+    price: 55,
+    quantity: 2,
+    category: "Watches"
+  }
+];
+
+/* Create a function "quantityByCategories" with one parameter "products".
+
+This function "dataByCategories" should return an object with keys equal to categories and values equal to sum of all quantities in each category.
+*/
+
+console.log(quantityByCategories(products));
+/* {
+  Accessories: 3,
+  Phones: 1,
+  Watches: 2
+} */
