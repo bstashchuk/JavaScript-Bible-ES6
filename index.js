9edc962c8a3704b978a1fdcbeda8e16a5ceb751d
Bogdan <bstashchuk@gmail.com>
2018-08-27 13:25:40 +0300

    Final

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const myNumbers = [10, 5, 7, 12, 20];
+/* 
+1. Sorting algorithm
+2. Qty of iterations
+*/
+const myNumbers = [10, 5, 79, 27, 50, 14, 27];
 
-// [5, 7, 10, 12, 20]
-
-console.log(myNumbers.sort());
-
-console.log(myNumbers.sort((a, b) => a - b));
+let i = 0;
+console.log(
+  myNumbers.sort((a, b) => {
+    console.log(a, b);
+    i++;
+    return a - b;
+  })
+);
+console.log("Qty of iterations is " + i);
