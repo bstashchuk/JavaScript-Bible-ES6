3f2253b406409b2cde9638dadce26a8df4688fb1
Bogdan <bstashchuk@gmail.com>
2018-09-02 08:33:17 +0300

    Final

---
+++ index.js
@@ -0,0 +1,22 @@
+/* Create a function "templateLiteral" with one parameter "num".
+
+Function is expected to return template literal. Sample function calls are below 
+*/
+
+// TEST 1
+const myNumber = 9;
+console.log(templateLiteral(myNumber));
+/* 
+Number is 9.
+This number is less than 10.
+Square root of this number is 3.
+*/
+
+// TEST 2
+const myAnotherNumber = 25;
+console.log(templateLiteral(myAnotherNumber));
+/* 
+Number is 25.
+This number is greater than 10.
+Square root of this number is 5.
+*/
