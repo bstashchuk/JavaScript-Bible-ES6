6f1c8d0ca8764177246a5eccd8bacceafa7d2386
Bogdan <bstashchuk@gmail.com>
2018-09-02 11:56:46 +0300

    Final

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 Function is expected to return template literal. Sample function calls are below.
 */
 
+const templateLiteral = num => `Number is ${num}
+This number is ${
+  num >= 10 ? "greater or equal" : "less"
+} than 10.
+Square root of this number is ${Math.sqrt(num)}`;
+
 // TEST 1
 const myNumber = 9;
 console.log(templateLiteral(myNumber));
@@ -17,6 +23,6 @@ const myAnotherNumber = 25;
 console.log(templateLiteral(myAnotherNumber));
 /* 
 Number is 25.
-This number is greater than 10.
+This number is greater than or equal 10.
 Square root of this number is 5.
 */
