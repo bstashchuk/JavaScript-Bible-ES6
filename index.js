b4d2cc6bc616f4de79cf77db777a4abaa4d08e8b
Bogdan <bstashchuk@gmail.com>
2018-09-04 20:33:01 +0300

    Fix

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const scores1 = [0, 1.5, 2.5, 3.7];
-const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2];
-const scores3 = [1.3, 2.5, 1.9];
-const scores4 = ["abc", 1.3, true, 2.5, 1.9];
-
 /*
 Create a function "meanScore" that will accept any quantity of the arguments, gather them into single array and return mean value of all arguments rounded to 2 decimal places.
 
@@ -13,6 +8,11 @@ Type of the returned value must be a "number".
 
 HINT: In this Challenge you should use both rest and spread operators.
 */
+
+const scores1 = [0, 1.5, 2.5, 3.7];
+const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2];
+const scores3 = [1.3, 2.5, 1.9];
+const scores4 = ["abc", 1.3, true, 2.5, 1.9];
 
 console.log(meanScore(/* all elements from "scores1" */)); // 1.93
 
