7dd0e82b2ba72c02f58b530f798b610cb05361f4
Bogdan <bstashchuk@gmail.com>
2018-09-05 09:00:39 +0300

    Final

--- index.js
+++ index.js
@@ -9,19 +9,32 @@ Type of the returned value must be a "number".
 HINT: In this Challenge you should use both rest and spread operators.
 */
 
+const meanScore = (...scores) => {
+  if (scores.every(score => typeof score === "number")) {
+    // calculate mean
+    return parseFloat(
+      scores
+        .reduce((avg, score) => {
+          return avg + score / scores.length;
+        }, 0)
+        .toFixed(2)
+    );
+  } else {
+    throw new Error(
+      "Supplied arguments must contain only numbers!"
+    );
+  }
+};
+
 const scores1 = [0, 1.5, 2.5, 3.7];
 const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2];
 const scores3 = [1.3, 2.5, 1.9];
 const scores4 = ["abc", 1.3, true, 2.5, 1.9];
 
-console.log(meanScore(/* all elements from "scores1" */)); // 1.93
+console.log(meanScore(...scores1)); // 1.93
 
-console.log(
-  meanScore(/* all elements from "scores1" and "scores2" */)
-); // 2.8
+console.log(meanScore(...scores1, ...scores2)); // 2.8
 
-console.log(
-  meanScore(/* all elements from "scores1" and "scores2" and "scores3" */)
-); // 2.59
+console.log(meanScore(...scores1, ...scores2, ...scores3)); // 2.59
 
-console.log(meanScore(/* all elements from "scores4" */)); // Supplied arguments must contain only numbers!
+console.log(meanScore(...scores4)); // Supplied arguments must contain only numbers!
