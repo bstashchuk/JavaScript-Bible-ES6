0a6c85dd23ba39d25d1bea63c78c5e1304818969
Bogdan <bstashchuk@gmail.com>
2018-09-14 10:12:22 +0300

    Fix

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /*
 Modify "personInfo" function to match console.log output at the end of the challenge.
+
+Object that is returned by "personInfo" function must contain only shorthand property names.
 */
 
 const personInfo = (/* parameters */) => {
