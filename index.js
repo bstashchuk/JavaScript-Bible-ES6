c4184985a09b2a51d6c0390b292c43e56f9bd028
Bogdan <bstashchuk@gmail.com>
2018-09-14 10:09:13 +0300

    Final

---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+Modify "personInfo" function to match console.log output at the end of the challenge.
+*/
+
+const personInfo = (/* parameters */) => {
+  /* return ... */
+};
+
+const person = {
+  name: "Alice",
+  age: 19,
+  location: {
+    country: "England",
+    city: "London"
+  }
+};
+
+console.log(personInfo(person));
+/*
+{
+  name: "Alice",
+  personAge: 19,
+  origin: "England",
+  homeCity: "London",
+  friendsQty: 0,
+  recordCreatedAt: *current year*
+}
+*/
