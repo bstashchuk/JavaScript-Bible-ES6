ab1cf76f8a29c24cb1383e09ae2d0a0021c65385
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-09-20 09:04:16 +0300

    Final

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+Create a function "minMax" that will accept any quantity of the arguments.
+
+This function should return array of two elements:
+1. First element in the array is minimal value among all arguments
+2. Second element in the array is maximal value among all arguments
+
+Use destructuring to parse results of the function calls.
+*/
+
+let min, max;
+/* call here "minMax" function with arguments 24, 5, 34, 10 */
+console.log(min, max); // 5, 34
+
+/* call here "minMax" function with arguments 18, 23, 103, 70, 80, 25 */
+console.log(min, max); // 18, 103
