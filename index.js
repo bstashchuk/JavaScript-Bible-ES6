36b8e68588c1491488bce7aab43359f1376b7ad9
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-09-28 18:31:32 +0300

    Final

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ Modify "personInfo" function to match console.log output at the end of the chall
 Object that is returned by "personInfo" function must contain only shorthand property names.
 */
 
-const personInfo = (/* parameters */) => {
-  /* return ... */
+const personInfo = ({
+  name,
+  age: personAge,
+  location: { country: origin, city: homeCity },
+  friendsQty = 0,
+  recordCreatedAt = new Date().getFullYear()
+}) => {
+  return {
+    name,
+    personAge,
+    origin,
+    homeCity,
+    friendsQty,
+    recordCreatedAt
+  };
 };
 
 const person = {
