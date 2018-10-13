a9a7803cca5985f2dbf849c2b8248e3e0eea27fa
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-10-13 08:05:21 +0300

    Final

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * CHALLENGE 2-1 TASK
+ *
+ * Create a button with "Click me" text on it.
+ *
+ * If you click on the button following actions
+ * should be performed:
+ * 1. Title of the button will be changed to following:
+ * "Button was clicked <NUMBER> times"
+ * 2. Log to the console message "Button was clicked"
+ *
+ * Use either "addEventListener" or "onclick" functions.
+ * Try to use regular function and arrow function as a
+ * callback function for functions above.
+ *
+ * Observe difference in behaviour and try to explain
+ * this difference.
+ */
