7f5f559ed974656d7a0e952c63767e9d80ec4fb1
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-10-13 07:56:06 +0300

    Final

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+/**
+ * CHALLENGE 1-3 SOLUTION
+ *
+ * DON'T EXECUTE THIS CODE!
+ * You need to answer following question without code exectution:
+ * "What will be printed to the console at the end of the code?"
+ *
+ * You should fill in files "compilation.js" and "execution.js"
+ * and add there all actions that JavaScript Engine performs on
+ * the Compilation and Execution phases in terms of variables
+ * declaration, initialization and assignment.
+ *
+ * Don't guess result!
+ */
+
 let b = 5;
 
 a = b;
