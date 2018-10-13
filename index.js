42082703708b47121d929723c1d1e858718447a3
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-10-13 07:50:15 +0300

    Final

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-/* Find all menu elements by selector ".nav-link"
-or by className "nav-link".
-
-Use "for" loop to iterate through all menu items
-Inside of the loop add "onclick" event handler to 
-each menu item.
-
-In the "onclick" event handler:
-1. Remove first "active" class from all
- menu elements
-2. Add "active" class to the clicked menu element
-3. Log to the console message with the name
- of the clicked menu item
-*/
+/**
+ * CHALLENGE 1-2 TASK
+ *
+ * Find all menu elements by selector ".nav-link"
+ * or by className "nav-link".
+ *
+ *  Use "for" loop to iterate through all menu items
+ * Inside of the loop add "onclick" event handler to
+ * each menu item.
+ *
+ * In the "onclick" event handler:
+ * 1. Remove first "active" class from all
+ * menu elements
+ * 2. Add "active" class to the clicked menu element
+ * 3. Log to the console message with the name
+ * of the clicked menu item
+ */
