763568b8b8f11a8f3f2b00230a1f20399fe3f40c
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-10-15 16:17:27 +0300

    Final

--- index.js
+++ index.js
@@ -5,8 +5,7 @@
  *
  * Add two custom methods to the new class:
  * 1. "sum" - it should return sum of all elements in the array
- * 2. "onlyNumbers" - it should return new array that will
- * contain only numbers from the source array
+ * 2. "onlyNumbers" - it should return new array that will contain only numbers from the source array
  *
  * Create several instances of the new "ExtendedArray" class and test both methods "sum" and "onlyNumbers"
  */
