c2f65885750eb7fa8e059aec8f799065d869b395
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-10-15 16:06:32 +0300

    Final

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 /**
  * CHALLENGE 7-4: TASK
  *
- * Create new class "ExtendedArray" that should extend
- * built-in "Array".
+ * Create new class "ExtendedArray" that should extend built-in "Array".
  *
  * Add two custom methods to the new class:
  * 1. "sum" - it should return sum of all elements in the array
  * 2. "onlyNumbers" - it should return new array that will
  * contain only numbers from the source array
  *
- * Create several instances of the new "ExtendedArray" class
- * and test both methods "sum" and "onlyNumbers"
+ * Create several instances of the new "ExtendedArray" class and test both methods "sum" and "onlyNumbers"
  */
