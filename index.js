761224f0251c41f8f70fc717c91aa95dbef717e7
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-10-15 16:57:28 +0300

    Final

--- index.js
+++ index.js
@@ -9,3 +9,46 @@
  *
  * Create several instances of the new "ExtendedArray" class and test both methods "sum" and "onlyNumbers"
  */
+
+class ExtendedArray extends Array {
+  sum() {
+    return this.reduce((sum, el) => sum + el);
+  }
+
+  onlyNumbers() {
+    return this.filter(el => typeof el === "number");
+  }
+}
+
+const firstInstance = new ExtendedArray(2, 3, 6, 10);
+
+console.log(firstInstance.sum());
+console.log(firstInstance.onlyNumbers()); // [2, 3, 6, 10]
+
+firstInstance.push("abc");
+
+console.log(firstInstance); // [2, 3, 6, 10, "abc"]
+
+console.log(firstInstance instanceof ExtendedArray); // true
+
+console.log(firstInstance instanceof Array); // true
+
+console.log(firstInstance instanceof Object); // true
+
+console.log(
+  firstInstance.__proto__ === ExtendedArray.prototype
+); // true
+
+const secondInstance = new ExtendedArray(
+  true,
+  "",
+  null,
+  2,
+  10,
+  false,
+  "abc"
+);
+
+const filteredArray = secondInstance.onlyNumbers();
+
+console.log(filteredArray); // [2, 10] - still instanceof ExtendedArray
