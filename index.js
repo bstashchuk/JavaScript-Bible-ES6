fd08f81e4319814188bd8f245133cb229c8fb2d5
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-10-16 12:06:37 +0300

    Start

---
+++ index.js
@@ -0,0 +1,29 @@
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clickedQty: 0 };
+  }
+
+  render() {
+    return React.createElement(
+      "button",
+      {
+        onClick: () =>
+          this.setState({
+            clickedQty: ++this.state.clickedQty
+          })
+      },
+      this.state.clickedQty > 0
+        ? `Button was clicked ${
+            this.state.clickedQty
+          } times`
+        : "Click me!!!"
+    );
+  }
+}
+
+const button1 = React.createElement(LikeButton);
+
+const buttonContainer1 = document.getElementById("button1");
+
+ReactDOM.render(button1, buttonContainer1);
