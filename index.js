b34bf135e50960597756fa5642b7686b47115b6f
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-10-16 09:31:50 +0300

    Final

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 /**
  * CHALLENGE 7-5: TASK
  *
- * Create new class "CustomArray" that should extend
- * built-in "Array".
+ * Create new class "CustomArray" that should extend built-in "Array".
  *
  * Add one custom method "customPush" to the new class.
  * This method will have one parameter "newElement".
@@ -14,10 +13,8 @@
  * 3. Log following line to the console:
  * "New element <ELEMENT> was just added to the array"
  *
- * Create instance of the new "CustomArray" class
- * and test new method "customPush" and compare it with "push"
+ * Create instance of the new "CustomArray" class and test new method "customPush" and compare it with "push"
  *
- * What will happen if name of the custom method
- * in the "CustomArray" class will be also "push"
- * instead of "customPush"? Try this.
+ * What will happen if name of the custom method in the "CustomArray" class will be also "push" instead of "customPush"?
+ * Try this.
  */
