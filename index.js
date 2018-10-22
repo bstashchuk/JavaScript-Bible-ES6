0e07fbcf81eab1552d641b86bf3cdf01f6ba0977
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-10-22 14:04:58 +0300

    Initial

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/**
+ * CHALLENGE 7-6 - TASK
+ *
+ * Add one more button with color "secondary".
+ * Also change styles of the existing and new buttons
+ * using property "variant".
+ *
+ * You can use one of the following values:
+ * "text", "flat", "outlined", "contained", "raised", "fab", "extendedFab"
+ */
 class MyButton extends React.Component {
   constructor(props) {
     super(props);
